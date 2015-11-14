4fd4c5c4826818f43daa42b90b9570d043a1a30c
microsheep <twmicrosheep@gmail.com>
2015-11-14 22:54:20 +0800

    fix template

--- FBxGIT/settings.py
+++ FBxGIT/settings.py
@@ -103,11 +103,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_file'),
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates').replace('\\', '/'),
+    os.path.join(BASE_DIR, 'static_file'), 
 )
 
 
