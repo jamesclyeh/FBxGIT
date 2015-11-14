4174b24c304a7ed0801e019a8866ecf2b87cdc21
Kbear_Lin <Kbear_Lin@htc.com>
2015-11-14 23:54:47 +0800

    add full_list function to return goods list

--- donate/views.py
+++ donate/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.template.response import TemplateResponse
 
+from donate.models import Goods
+
+import SQLiteUtil
+
 # Create your views here.
 
 def index(request):
     return TemplateResponse(request, 'index.html', {})
 
 def full_list(request):
+    dic = {}
+    dic['goods'] = Goods.objects.all()
+    print(dic)
     return TemplateResponse(request, 'full_list.html', {})
 
 def fblogin(request):
