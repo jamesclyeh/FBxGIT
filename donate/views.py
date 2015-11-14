ab3b91279b6990eee6faaa8fb5600519b3775bd4
DanielChien <whitetiger50512@gmail.com>
2015-11-15 03:24:45 +0800

    add_list.html update in views.py

--- donate/views.py
+++ donate/views.py
@@ -19,6 +19,8 @@ def full_list(request):
 
 def add_list(request):
     dic = {}
+    dic['cats'] = enumerate(set(x.category for x in Goods.objects.all()));
+
     return TemplateResponse(request, 'add_list.html', dic)
 
 def upload(request):
